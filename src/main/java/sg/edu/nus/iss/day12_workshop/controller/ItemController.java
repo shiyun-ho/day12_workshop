839737cfb07e562c045e908a9d592e96cb1172dd
shiyun-ho <serene.hoshiyun@gmail.com>
2023-02-07 13:23:37 +0800

    final code

--- src/main/java/sg/edu/nus/iss/day12_workshop/controller/ItemController.java
+++ src/main/java/sg/edu/nus/iss/day12_workshop/controller/ItemController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.*;
 
 
 import sg.edu.nus.iss.day12_workshop.service.ItemService;
 import sg.edu.nus.iss.day12_workshop.model.Item;
 import org.springframework.ui.Model;
+import java.util.stream.Collectors;
 
 //annotate to know that it is controller
 @Controller
@@ -28,4 +31,17 @@ public class ItemController {
         model.addAttribute("cartItems", items);
         return "cartList";
     }
+
+    //when /prada, only shows prada
+    @GetMapping("{itemname}")
+    public String filteredCart(@PathVariable("itemname") String itemname, Model model){
+        //filter away the records
+        List<Item> items = itmSvc.retrieveItemList(); 
+        List<Item> foundItem = items.stream().filter(i -> i.getItemName().equalsIgnoreCase(itemname)).
+        collect(Collectors.toList());
+        model.addAttribute("cartItems", foundItem);
+        
+        return "cartList";
+    }
+
 }
